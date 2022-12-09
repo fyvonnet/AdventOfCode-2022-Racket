2816e7a0879744e96c2369260451910b5ab18771
Franck YVONNET <franck@yvonnet.fr>
2022-12-09 13:40:12 +0100

    refactoring

--- day09.rkt
+++ day09.rkt
@@ -1,15 +1,18 @@
 #lang racket
 
-(define (read-input port)
+(define (read-input port [coords (list '(0 0))])
   (let ([line (read-line port)])
     (if (eof-object? line)
-      '()
-      (cons
-        (let ([d (string-ref line 0)])
-          (cons
-            (case d [(#\U) '( 0  1)] [(#\D) '( 0 -1)] [(#\L) '(-1  0)] [(#\R) '( 1  0)])
-            (string->number (substring line 2))))
-        (read-input port)))))
+      (reverse coords)
+      (let
+        ([move 
+           (case (string-ref line 0)
+             [(#\U) '( 0  1)] [(#\D) '( 0 -1)] [(#\L) '(-1  0)] [(#\R) '( 1  0)])]
+         [steps (string->number (substring line 2))])
+        (let rec ([s steps] [cs coords])
+          (if (zero? s)
+            (read-input port cs)
+            (rec (sub1 s) (cons (map + (car cs) move) cs))))))))
 
 (define (get-tail-move rp)
   (match rp
@@ -22,21 +25,15 @@
                '(99 99) '(-1 -1) '( 0 -1) '(+1 -1) '(99 99))
        (+ (* 5 (- 2 y)) (+ 2 x)))]))
 
+(define (new-follower-coord leader follower)
+  (let ([rel-pos (map - leader follower)])
+    (map + follower (get-tail-move rel-pos))))
 
-(define (solve input [set (set)] [head-coord '(0 0)] [tail-coord '(0 0)])
-  (if (null? input)
-    (length (set->list set))
-    (match (car input)
-      [(cons dir steps)
-       (let rec ([n steps] [s set] [hc head-coord] [tc tail-coord])
-         (if (zero? n)
-           (solve (cdr input) s hc tc)
-           (let*
-             ([new-hc (map + dir hc)]
-              [rel-pos (map - new-hc tc)]
-              [new-tc (map + tc (get-tail-move rel-pos))])
-             (rec (sub1 n) (set-add s new-tc) new-hc new-tc))))])))
-
-(let ([input (call-with-input-file "inputs/day09" read-input)])
-  (displayln (solve input)))
-
+(let ([head-coords (call-with-input-file "inputs/day09" read-input)])
+  (for/fold
+    ([tail-coord '(0 0)]
+     [coords-set (set '(0 0))]
+     #:result (length (set->list coords-set)))
+    ([head-coord head-coords])
+    (let ([new-tail-coord (new-follower-coord head-coord tail-coord)])
+      (values new-tail-coord (set-add coords-set new-tail-coord)))))
