07f68c5629fe612b338e585091098ddc4eb4538e
Franck YVONNET <franck@yvonnet.fr>
2022-12-10 08:24:45 +0100

    use for loop for part 2

--- day10.rkt
+++ day10.rkt
@@ -15,16 +15,12 @@
              (read-input port 1 val new-x (cons new-x lst)))])))
     (read-input port (sub1 n) add x (cons x lst))))
 
-(define (solve-part-two input [cycles (in-naturals)])
-  (unless (null? input)
-    (let ([hpos (remainder (stream-first cycles) 40)])
-      (display (if (and (>= hpos (sub1 (car input))) (<= hpos (add1 (car input)))) "##" "  "))
-      (when (= 39 hpos) (display "\n"))
-      (solve-part-two (cdr input) (stream-rest cycles)))))
-
 (let*
   ([input (call-with-input-file "inputs/day10" read-input)]
    [input-vec (list->vector input)])
   (displayln (apply + (map (lambda (x) (* x (vector-ref input-vec (sub1 x)))) '(20 60 100 140 180 220))))
-  (solve-part-two input))
+  (for ([sprite-pos input] [c (in-range 240)])
+    (let ([hpos (remainder c 40)])
+      (display (if (and (>= hpos (sub1 sprite-pos)) (<= hpos (add1 sprite-pos))) "##" "  "))
+      (when (= 39 hpos) (display "\n")))))
 
