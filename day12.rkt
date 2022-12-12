711047e454e41cbfcdcb81460b95ae29e0085a84
Franck YVONNET <franck@yvonnet.fr>
2022-12-12 19:20:59 +0100

    part 2

--- day12.rkt
+++ day12.rkt
@@ -3,43 +3,60 @@
 (require "queue.rkt")
 (require "matrix.rkt")
 
-(define (read-input port [row 0] [start #f] [end #f] [heights '()])
+(define (read-input port [row 0] [start #f] [end #f] [heights '()] [start-points '()])
   (let ([line (read-line port)])
     (if (eof-object? line)
-      (values (list->matrix (reverse heights)) start end)
-      (let rec ([lst (string->list line)] [s start] [e end] [col 0] [hs '()])
+      (values (list->matrix (reverse heights)) start end start-points)
+      (let rec ([lst (string->list line)] [s start] [e end] [col 0] [hs '()] [sp start-points])
         (if (null? lst)
-          (read-input port (add1 row) s e (cons (reverse hs) heights))
-          (match (car lst)
-            [#\S (rec (cdr lst) (list col row) e (add1 col) (cons 0 hs))]
-            [#\E (rec (cdr lst) s (list col row) (add1 col) (cons 26 hs))]
-            [c (rec (cdr lst) s e (add1 col) (cons (- (char->integer c) 97) hs))]))))))
+          (read-input port (add1 row) s e (cons (reverse hs) heights) sp)
+          (let-values 
+            ([(new-s new-e new-hs new-sp)
+              (let
+                ([coord (list col row)])
+                (match (car lst)
+                  [#\S (values coord e (cons  0 hs) (cons coord sp))]
+                  [#\E (values s coord (cons 26 hs) sp)]
+                  [c   (values s e (cons (- (char->integer c) 97) hs) (if (char=? c #\a) (cons coord sp) sp))]))])
+            (rec (cdr lst) new-s new-e (add1 col) new-hs new-sp)))))))
 
-(define (fill-queue heightmap not-visited pos height steps)
+(define (fill-queue heightmap not-visited pos height steps trail)
   (lambda (move queue)
-    (match (map + pos move)
-      [(list c r)
-       (match (matrix-ref-safe heightmap c r #f)
-         [#f queue]
-         [new-height
-           (if (and (<= new-height (add1 height)) (matrix-ref not-visited c r))
-             (begin
-               (matrix-set! not-visited c r #f)
-               (queue-snoc queue (list (list c r) (add1 steps) new-height)))
-             queue)])])))
+    (let
+      ([new-pos (map + pos move)])
+      (match new-pos
+        [(list c r)
+         (match (matrix-ref-safe heightmap c r #f)
+           [#f queue]
+           [new-height
+             (if (and (<= new-height (add1 height)) (matrix-ref not-visited c r))
+               (begin
+                 (matrix-set! not-visited c r #f)
+                 (queue-snoc queue (list new-pos (add1 steps) new-height (cons new-pos trail))))
+               queue)])]))))
 
-(let-values
-  ([(heightmap start end) (call-with-input-file "inputs/day12" read-input)])
-  (let ([not-visited (make-matrix (matrix-cols heightmap) (matrix-rows heightmap) #t)])
-    (displayln
-      (let rec ([queue (queue-snoc (empty-queue) (list start 0 0))])
+(define (reach-end heightmap end)
+  (lambda (start) 
+    (let
+      ([not-visited (make-matrix (matrix-cols heightmap) (matrix-rows heightmap) #t)])
+      (let rec ([queue (queue-snoc (empty-queue) (list start 0 0 '()))])
         (match (queue-head queue)
-          [(list pos steps height)
+          [(list pos steps height trail)
            (if (equal? pos end)
              steps
-             (rec
-               (foldl
-                 (fill-queue heightmap not-visited pos height steps)
-                 (queue-tail queue)
-                 (list '(1 0) '(-1 0) '(0 -1) '(0 1)))))])))))
+             (let
+               ([new-queue
+                  (foldl
+                    (fill-queue heightmap not-visited pos height steps trail)
+                    (queue-tail queue)
+                    (list '(1 0) '(-1 0) '(0 -1) '(0 1)))])
+               (if (queue-empty? new-queue)
+                 #f
+                 (rec new-queue))))])))))
+
+(let-values
+  ([(heightmap start end start-points) (call-with-input-file "inputs/day12" read-input)])
+  (let ([func (reach-end heightmap end)])
+    (displayln (func start))
+    (displayln (apply min (filter identity (map func start-points))))))
 
