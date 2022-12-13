32fc0b588644697314376f50b4a701ec4bfb3a85
Franck YVONNET <franck@yvonnet.fr>
2022-12-13 10:09:04 +0100

    part 2

--- day13.rkt
+++ day13.rkt
@@ -1,10 +1,11 @@
 #lang racket
 
-(define (compare-lists left right)
+(define (compare-packets left right)
+  (let ([null-left (null? left)] [null-right (null? right)])
   (cond
-    [(and (null?  left) (not (null? right))) #t]
-    [(and (null? right) (not (null?  left))) #f]
-    [(and (null? left) (null? right)) 0]
+    [(and null-left  (not null-right)) #t]
+    [(and null-right (not null-left )) #f]
+    [(and null-left null-right) 0]
     [else
       (match-let
         ([(list-rest fst-left  rst-left ) left ]
@@ -12,31 +13,41 @@
         (match
           (if (list? fst-left)
             (if (list? fst-right)
-              (compare-lists fst-left fst-right)
-              (compare-lists fst-left (list fst-right)))
+              (compare-packets fst-left fst-right)
+              (compare-packets fst-left (list fst-right)))
             (if (list? fst-right)
-              (compare-lists (list fst-left) fst-right)
+              (compare-packets (list fst-left) fst-right)
               (cond
                 [(< fst-left fst-right) #t]
                 [(> fst-left fst-right) #f]
                 [else 0])))
-          [0 (compare-lists rst-left rst-right)]
-          [b b]))]))
+          [0 (compare-packets rst-left rst-right)]
+          [b b]))])))
 
-(define (solve lst [n 1])
-  (match lst
+(define (solve-part-one packets [n 1])
+  (match packets
     ['() 0]
-    [(list-rest (list l r) rst)
-     (+ (if (compare-lists l r) n 0) (solve rst (add1 n)))]))
+    [(list-rest left right rst)
+     (+
+       (if (compare-packets left right) n 0)
+       (solve-part-one rst (add1 n)))]))
 
-(define (read-input port)
-  (cons
-    (map
-      (lambda (str) (read (open-input-string (string-replace str "," " "))))
-      (list (read-line port) (read-line port)))
-    (if (eof-object? (read-line port))
-      '()
-      (read-input port))))
+(define (evaluate str)
+  (read (open-input-string (string-replace str "," " "))))
 
-(displayln (solve (call-with-input-file "inputs/day13" read-input)))
+(define (read-input port [packets null])
+  (let ([line (read-line port)])
+    (cond
+      [(eof-object? line) (reverse packets)]
+      [(non-empty-string? line) (read-input port (cons (evaluate line) packets))]
+      [else (read-input port packets)])))
+
+(let*
+  ([packets (call-with-input-file "inputs/day13" read-input)]
+   [divider-packets (map evaluate '("[[2]]" "[[6]]"))]
+   [packets-vec (list->vector (sort (append divider-packets packets) compare-packets))])
+  (printf
+    "~a~%~a~%"
+    (solve-part-one packets)
+    (apply * (map (λ (p) (add1 (vector-member p packets-vec))) divider-packets))))
 
